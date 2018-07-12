e8db9366960546cdad168acfecd88afff11ce676
DESKTOP-84IKM2Q\Desarrollo <ketrujillo08@gmail.com>
2018-07-11 19:05:48 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:stuSBa9AM1@ds131971.mlab.com:31971/cafeudemy';
+    urlDB = process.env.MONGOURI;
 }
 process.env.URLDB = urlDB;
